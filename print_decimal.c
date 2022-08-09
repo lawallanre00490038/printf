82dc473528ab0c6776be1a45cc6f00c671e04b2a
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-08-09 20:24:41 +0100

    Update print_decimal.c

--- print_decimal.c
+++ print_decimal.c
@@ -6,15 +6,19 @@
 * Return: Always 0
 */
 
-int print_decimal(int value){
-int count = 0;
-if (value < 0){
-count += _putchar('-');
-value *= -1;
-}
-if (value / 10){
-count += print_decimal(value / 10);
-}
-count += _putchar((value % 10) + '0');
-return (count);
+int
+print_decimal (int value)
+{
+  int count = 0;
+  if (value < 0)
+    {
+      count += _putchar ('-');
+      value *= -1;
+    }
+  if (value / 10)
+    {
+      count += print_decimal (value / 10);
+    }
+  count += _putchar ((value % 10) + '0');
+  return (count);
 }
